5d41c4d8496043730ce6a36a62039ddd46eec3e9
muhammadjunaidkhalid <mjk22071998@gmail.com>
2022-02-05 15:42:24 +0500

    Splash Animation Added

---
+++ app/src/main/java/com/example/dmr/medicalrep/activities/SplashActivity.java
@@ -0,0 +1,47 @@
+package com.example.dmr.medicalrep.activities;
+
+
+import android.animation.Animator;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.example.dmr.medicalrep.R;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    LottieAnimationView animationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        animationView=findViewById(R.id.animationView);
+        animationView.addAnimatorListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                finish();
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                finish();
+            }
+        });
+    }
+}

49e85174ae1d633bcce78a0c1f1c3a458165f98c
muhammadjunaidkhalid <mjk22071998@gmail.com>
2022-04-05 15:46:43 +0500

    Rejected request display done

--- app/src/main/java/com/example/dmr/medicalrep/fragments/RejectedFragment.java
+++ app/src/main/java/com/example/dmr/medicalrep/fragments/RejectedFragment.java
@@ -1,9 +1,13 @@
 package com.example.dmr.medicalrep.fragments;
 
+import static android.content.Context.MODE_PRIVATE;
+
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
@@ -11,7 +15,11 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.dmr.medicalrep.R;
+import com.example.dmr.medicalrep.adapters.RequestAdapter;
 import com.example.dmr.medicalrep.model.Request;
+import com.example.dmr.medicalrep.utils.SessionManager;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -34,6 +42,29 @@ public class RejectedFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_rejected, container, false);
         rejected=view.findViewById(R.id.reject);
+        sharedPreferences=getActivity().getSharedPreferences("File",MODE_PRIVATE);
+        if (sharedPreferences.getBoolean("rep",false)) {
+            task = firestore.collection("Request").whereEqualTo("from", SessionManager.getUser(getContext()).get(SessionManager.CNIC).toString()).whereEqualTo("status","Rejected").get();
+        } else {
+            task = firestore.collection("Request").whereEqualTo("to", SessionManager.getUser(getContext()).get(SessionManager.CNIC).toString()).whereEqualTo("status","Rejected").get();
+        }
+        task.addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                requests=queryDocumentSnapshots.toObjects(Request.class);
+                updateUI();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                e.printStackTrace();
+            }
+        });
         return view;
+    }
+
+    void updateUI(){
+        rejected.setAdapter(new RequestAdapter(requests));
+        rejected.setLayoutManager(new LinearLayoutManager(getContext()));
     }
 }

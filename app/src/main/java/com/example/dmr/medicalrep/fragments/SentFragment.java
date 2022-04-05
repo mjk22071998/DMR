18dfd10fe54c163478bf6ed9e09e1fce7a5d9ddd
muhammadjunaidkhalid <mjk22071998@gmail.com>
2022-04-05 16:26:24 +0500

    Sent request display done

--- app/src/main/java/com/example/dmr/medicalrep/fragments/SentFragment.java
+++ app/src/main/java/com/example/dmr/medicalrep/fragments/SentFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
@@ -12,6 +13,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.dmr.medicalrep.R;
+import com.example.dmr.medicalrep.adapters.RequestAdapter;
 import com.example.dmr.medicalrep.model.Request;
 import com.example.dmr.medicalrep.utils.SessionManager;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -55,6 +57,7 @@ public class SentFragment extends Fragment {
     }
 
     void updateUI(){
-
+        sent.setAdapter(new RequestAdapter(requests));
+        sent.setLayoutManager(new LinearLayoutManager(getContext()));
     }
 }
